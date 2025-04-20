e3e9c904c9cb5812127fe7b169512ac39f8dbd81
Son Fang <sonphan18071999@gmail.com>
2025-04-20 21:47:09 +0700

    Refactor code

---
+++ avnon-app/src/app/model/date.model.ts
@@ -0,0 +1,8 @@
+
+export interface DateRangeOutput {
+    startDate: Date;
+    endDate: Date;
+    startMonth: string;
+    endMonth: string;
+  }
+  
--- avnon-app/src/app/pages/budget-calculator/budget-calculator.component.ts
+++ avnon-app/src/app/pages/budget-calculator/budget-calculator.component.ts
@@ -1,14 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { BudgetHeaderComponent } from '../../features/budget-header/budget-header.component';
 import { ExelFileBudgetComponent } from '../../features/exel-file-budget/exel-file-budget.component';
-
-// Define the DateRangeOutput interface
-interface DateRangeOutput {
-  startDate: Date;
-  endDate: Date;
-  startMonth: string;
-  endMonth: string;
-}
+import { DateRangeOutput } from '../../model/date.model';
 
 @Component({
   selector: 'app-budget-calculator',
@@ -21,7 +14,6 @@ export class BudgetCalculatorComponent {
   @ViewChild(ExelFileBudgetComponent) exelFileBudget!: ExelFileBudgetComponent;
 
   onDateRangeChanged(dateRange: DateRangeOutput): void {
-    // If the exel-file-budget component is initialized, call its method
     if (this.exelFileBudget) {
       this.exelFileBudget.onDateRangeChanged(dateRange);
     }
